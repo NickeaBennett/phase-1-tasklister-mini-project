569e86fa10cbe7873e4509da81bf953acb8e7ea4
NickeaBennett <nickeabennett@gmail.com>
2022-04-28 12:23:40 -0400

    Added priority dropdown function

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector('form')
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    buildToDo(e.target.new_task_description.value);
+    
+    buildToDo(e.target.new_task_description.value, e.target.children[3].value);
+    // buildToDo(e.target.new_task_description.value);
     form.reset();
   })
 });
 
-function buildToDo(todo) {
+function buildToDo(todo, color) {
   let pTag = document.createElement('p');
   let btn = document.createElement('button');
   btn.addEventListener('click', handleDelete);
   btn.textContent = "X";
   pTag.textContent = todo;
+  pTag.style.backgroundColor=color;
   pTag.append(btn);
   document.querySelector('#tasks').append(pTag);
 }
@@ -21,3 +24,12 @@ function buildToDo(todo) {
 function handleDelete(e) {
   e.target.parentNode.remove();
 }
+
+// function changeColor(e) {
+//     console.log(e);
+//     let color = e.value;
+//     document.getElementById('p').style.backgroundColor=color;
+//     // document.getElementById('tasks').style.backgroundColor=color;
+//     // e.target.pTag.append(color);
+
+// }
