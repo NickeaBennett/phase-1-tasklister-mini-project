791cef99c16bf031c6dcf1b7da9e12cf9caf66a1
NickeaBennett <nickeabennett@gmail.com>
2022-04-27 20:56:49 -0400

    Added function to collect user input from task description

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(e.target.new_task_description.value);
+  })
 });
+
+function handleToDo(todo) {
+  let p = document.createElement('p');
+  p.textContent = todo;
+  console.log(p);
+}
